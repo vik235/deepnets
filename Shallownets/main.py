15254d617aa1ced1c79d6e1eed2333158e54cf8d
Vivek Gupta (DYNAMICS) <vigupta@microsoft.com>
2019-07-09 16:56:58 -0700

    Grad descent on different learnign rates.

--- Shallownets/main.py
+++ Shallownets/main.py
@@ -82,3 +82,21 @@ plt.ylabel('cost')
 plt.xlabel('iterations (per hundreds)')
 plt.title("Learning rate =" + str(d["learning_rate"]))
 plt.show()
+
+learning_rates = [0.01, .001, .0001]
+models = {}
+for i in learning_rates:
+    print('leaenign rate is ' + str(i))
+    models[str(i)] = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations = 2000,
+           learning_rate = i, print_cost = False)
+    print ('\n' + "-------------------------------------------------------" + '\n')
+    
+for i in learning_rates:
+    plt.plot(np.squeeze(models[str(i)]["costs"]), label= str(models[str(i)]["learning_rate"]))
+    plt.ylabel('cost')
+    plt.xlabel('iterations (hundreds)')
+    legend = plt.legend(loc='upper center', shadow=True)
+    frame = legend.get_frame()
+    frame.set_facecolor('0.90')
+    plt.show()
+    
